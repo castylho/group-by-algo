c2f83e1e29daad69cc6881def557b97601f96763
castylho <castylho.dev@gmail.com>
2022-11-27 10:28:49 +0000

    updating state and showing data from api grouped by userId

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,37 @@
 import React, { useEffect, useState } from "react";
+import { APIRes, PostByUser, State } from "./types";
+import { formatData } from "./utils";
 
 const URL = "https://jsonplaceholder.typicode.com/posts";
 
 function App() {
-  const [state, setState] = useState({
+  const [state, setState] = useState<State>({
     postsByUser: {},
   });
 
   const fetchPosts = async () => {
     const data = await fetch(URL);
-    const jsonData = await data.json();
-    console.log(jsonData);
-
-    // setState({ postsByUser: formatData(jsonData) });
+    const jsonData: APIRes[] = await data.json();
+    setState({ postsByUser: formatData(jsonData) });
   };
 
   useEffect(() => {
     fetchPosts();
   }, []);
+
+  const renderPost = (propName: string, obj: PostByUser) => {
+    if (!obj.hasOwnProperty(propName)) {
+      return null;
+    }
+
+    return obj[propName].map((post) => (
+      <div key={`${post.userId}-${post.id}-${post.title}`}>
+        <p>PostId - {post.id}</p>
+        <p>{post.title}</p>
+        <span>{post.body}</span>
+      </div>
+    ));
+  };
 
   // Add MUI (List, ListItem)
   // onclick function to open posts from userId
@@ -25,7 +39,14 @@ function App() {
   return (
     <>
       <h1>Fetch data from JSON Placeholder API</h1>
-      {JSON.stringify(state.postsByUser)}
+      <ul>
+        {Object.keys(state.postsByUser).map((userId, index) => (
+          <li key={`${index}.${userId}`}>
+            UserId {userId}
+            {renderPost(userId, state.postsByUser)}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
