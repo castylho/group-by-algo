0a3a766e21c23aa130498667c30b5ddfcf2fa751
castylho <castylho.dev@gmail.com>
2022-11-27 10:24:25 +0000

    removed not used files, added initial state and request to API

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+
+const URL = "https://jsonplaceholder.typicode.com/posts";
 
 function App() {
+  const [state, setState] = useState({
+    postsByUser: {},
+  });
+
+  const fetchPosts = async () => {
+    const data = await fetch(URL);
+    const jsonData = await data.json();
+    console.log(jsonData);
+
+    // setState({ postsByUser: formatData(jsonData) });
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  // Add MUI (List, ListItem)
+  // onclick function to open posts from userId
+  // selected prop to highlight userId (selectedUserId === userId)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Fetch data from JSON Placeholder API</h1>
+      {JSON.stringify(state.postsByUser)}
+    </>
   );
 }
 
