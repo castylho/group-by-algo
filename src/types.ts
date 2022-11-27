faae6633fe636b3534ec538d68e430e4f47f43e4
castylho <castylho.dev@gmail.com>
2022-11-27 10:27:06 +0000

    added types

---
+++ src/types.ts
@@ -0,0 +1,25 @@
+export type APIRes = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+};
+
+export type Post = {
+  postId: number;
+  title: string;
+  body: string;
+};
+
+export type User = {
+  userId: number;
+  post: Post;
+};
+
+export type PostByUser = {
+  [index: string]: APIRes[];
+};
+
+export type State = {
+  postsByUser: PostByUser | {};
+};
